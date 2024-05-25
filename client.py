f52b4e68765ca0e358fcbca52ea11af4af7ab866
SelimV <selim.virtanen@gmail.com>
2024-05-25 20:29:57 +0300

    Send UDP packets

--- client.py
+++ client.py
@@ -1,5 +1,10 @@
 from pynput.keyboard import Key, Listener
 from time import sleep
+import socket
+import sys
+
+IPS_ARDUINO = ["192.168.80.172", "192.168.80.97"]
+PORT_ARDUINO = 1234
 
 
 class State:
@@ -7,7 +12,7 @@ class State:
     space_down = False
 
 
-def main():
+def main(ip_arduino):
     state = State()
 
     def key_press(key):
@@ -25,14 +30,26 @@ def main():
     listener = Listener(on_press=key_press, on_release=key_lift)
     listener.start()
 
+    socket_udp = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
     i = 0
     while state.running:
         if state.space_down:
             i += 1
             print(i)
+        else:
+            i = 0
+
+        socket_udp.sendto(
+            bytes(f"Spaces in a row {i}", "utf-8"), (ip_arduino, PORT_ARDUINO)
+        )
         sleep(0.1)
 
 
 if __name__ == "__main__":
-    print("Hello")
-    main()
+    if len(sys.argv) > 1:
+        ip_arduino = IPS_ARDUINO[int(sys.argv[1])]
+    else:
+        ip_arduino = IPS_ARDUINO[0]
+    print(f"Print connecting to {ip_arduino}")
+    main(ip_arduino)
