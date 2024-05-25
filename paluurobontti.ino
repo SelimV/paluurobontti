73e1ea3c7a2b73db370d8806faf600f8a078f20d
wforbom <christopher.forbom@aalto.fi>
2024-05-26 00:27:01 +0300

    Added control bases.

--- paluurobontti.ino
+++ paluurobontti.ino
@@ -6,6 +6,14 @@ AsyncUDP udp;
 float speed = 0;
 float turn = 0;
 
+int motor1Pin1 = 27; 
+int motor1Pin2 = 26; 
+int enable1Pin = 14;
+
+int motor2Pin1 = 33; 
+int motor2Pin2 = 32; 
+int enable2Pin = 35;
+
 void setup()
 {
     pinMode(LED_BUILTIN, OUTPUT);
@@ -34,6 +42,12 @@ void setup()
       });
     }
     Serial.println("setup complete");
+
+    pinMode(motor1Pin1, OUTPUT);
+    pinMode(motor1Pin2, OUTPUT);
+    pinMode(enable1Pin, OUTPUT);
+    ledcSetup(pwmChannel, freq, resolution);
+    ledcAttachPin(enable1Pin, pwmChannel);
 }
 
 void loop()
@@ -47,4 +61,24 @@ void loop()
   digitalWrite(LED_BUILTIN, LOW);
 
   delay(100);
+
+  //Serial.println("Moving Backwards");
+  //digitalWrite(motor1Pin1, HIGH);
+  //digitalWrite(motor1Pin2, LOW);
+
+  //digitalWrite(motor1Pin1, HIGH);
+  //digitalWrite(motor1Pin2, LOW);
+
+  //while (dutyCycle <= 255){
+  //  ledcWrite(pwmChannel, dutyCycle);
+  //  Serial.print("Forward with duty cycle: ");
+  //  Serial.println(dutyCycle);
+  //  dutyCycle = dutyCycle + 5;
+  //  delay(500);
+  ledcWrite(pwmChannel, dutyCycle);
+  analogWrite(motor1Pin1, )
+  analogWrite(motor1Pin2, min())
+  analogWrite(motor2Pin2, max())
+  analogWrite(motor2Pin2, min())
+  
 }
