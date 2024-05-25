abdf6e9012cb184a55a4cb45499a4213a82eb710
SelimV <selim.virtanen@gmail.com>
2024-05-25 15:23:01 +0300

    Connect WiFi

--- paluurobontti.ino
+++ paluurobontti.ino
@@ -1,6 +1,22 @@
+#include "WiFi.h"
+#include "wifi_credentials.h"
+
 void setup()
 {
     pinMode(LED_BUILTIN, OUTPUT);
+    WiFi.begin(ssid, password);
+    Serial.begin(115200);
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(500);
+        Serial.println("Connecting to WiFi..");
+    }
+    Serial.println("Connected to the WiFi network");
+    // Print local IP address and start web server
+    Serial.println("");
+    Serial.println("WiFi connected.");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
 }
 
 void loop()
@@ -8,7 +24,7 @@ void loop()
 
     digitalWrite(LED_BUILTIN, HIGH);
 
-    delay(1000);
+    delay(500);
 
     digitalWrite(LED_BUILTIN, LOW);
 
